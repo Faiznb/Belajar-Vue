ada1e9f574aa944af291ee387f869c7d308b72c2
unknown <faiznurbudi@gmail.com>
2024-08-15 14:28:15 +0700

    Section 6:1-13 Complete

---
+++ Section 6/1-13/src/components/User.vue
@@ -0,0 +1,35 @@
+<template>
+  <button type="button" @click="onClickAge">update age</button>
+  <button type="button" @click="ageChangeFn(3)">update age CB</button>
+  <p>The User is {{ age }} years old</p>
+  <p>{{ ageDoubled }}</p>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: {
+    age: {
+      type: Number,
+      //   required: true,
+      //   default: 20,
+      validator(value) {
+        // this.onClickAge();
+        return value < 130;
+      },
+    },
+    ageChangeFn: Function,
+  },
+  emits: ["age-change"],
+  computed: {
+    ageDoubled() {
+      return this.age * 2;
+    },
+  },
+  methods: {
+    onClickAge() {
+      this.$emit("age-change", 3);
+    },
+  },
+};
+</script>
