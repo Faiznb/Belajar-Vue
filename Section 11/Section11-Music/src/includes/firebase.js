0915dc9e9766324f40eb8acbf69a4cc91c898d82
unknown <faiznurbudi@gmail.com>
2024-08-17 14:14:25 +0700

    section 11 complete

--- Section 11/Section11-Music/src/includes/firebase.js
+++ Section 11/Section11-Music/src/includes/firebase.js
@@ -9,13 +9,7 @@ import {
 import { getFirestore, doc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBW4921E2UzbeDU_wha9tIVd0PPS0-T4sA',
-  authDomain: 'music-b52db.firebaseapp.com',
-  projectId: 'music-b52db',
-  storageBucket: 'music-b52db.appspot.com',
-  messagingSenderId: '71550116399',
-  appId: '1:71550116399:web:f606cb7e61c6759bbf9cda',
-  measurementId: 'G-C2V9ZCWLXN'
+  
 }
 
 const app = initializeApp(firebaseConfig)
