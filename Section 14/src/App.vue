c3e14bbe40bf052b10a40c35edb9d2cdc3d7d992
unknown <faiznurbudi@gmail.com>
2024-08-25 13:38:47 +0700

    Section 14 complete

---
+++ Section 14/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <AppHeader />
+
+  <RouterView v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </RouterView>
+  <AppPlayer />
+  <AppAuth />
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppAuth from './components/AppAuth.vue'
+import AppPlayer from './components/AppPlayer.vue'
+import { mapWritableState } from 'pinia'
+import useUserStore from '@/stores/user'
+import { auth } from './includes/firebase'
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppAuth,
+    AppPlayer
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ['userLoggedIn'])
+  },
+  created() {
+    if (auth.currentUser) {
+      this.userLoggedIn = true
+    }
+  }
+}
+</script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
